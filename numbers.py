d804385fe9da3b69c56816c861d0ca5b93ae279f
JayodRajapaksha <65191979+JayodRajapaksha@users.noreply.github.com>
2022-09-10 20:50:47 +0530

    Add files via upload

---
+++ numbers.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#Scientific_form
+
+print("{:e}".format(23.123))
+
+
+# In[ ]:
+
+
+#Error,Abs_error,Rel_error,Percentage_error
+
+x = 0.00012887765
+xstar = 0.00012897766
+error = x - xstar
+print("Error", error)
+print("Abs Error", abs(error))
+print("Relative Error", abs(error/x))
+print("Percentage Error", abs(error/x)*100)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
